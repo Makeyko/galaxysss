517339ef992129c8cc4a20b833d3e534ab962413
Dmitriy Mukhortov <dram1008@yandex.ru>
2015-05-25 15:46:28 +0300

    init

---
+++ config/mailer.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'facebook'  => [
+        'class'        => 'app\services\authclient\Facebook',
+        'clientId'     => '1114239771926233',
+        'clientSecret' => '51ae4b3deed4a2c8d35356069aff55ad',
+    ],
+    'vkontakte' => [
+        'class'        => 'app\services\authclient\VKontakte',
+        'clientId'     => '4896568',
+        'clientSecret' => 'lQD6kk2VshQlxEaw27Pw',
+    ],
+];
--- config/web.php
+++ config/web.php
@@ -41,13 +41,7 @@ $config = [
         'mailer'               => [
             'class'            => 'yii\swiftmailer\Mailer',
             'useFileTransport' => false,
-            'transport'        => [
-                'class'    => 'Swift_SmtpTransport',
-                'host'     => 'smtp.timeweb.ru',
-                'port'     => '25',
-                'username' => 'no-reply@galaxysss.ru',
-                'password' => 'no-reply1',
-            ],
+            'transport'        => require(__DIR__ . '/mailerTransport.php'),
         ],
         'log'                  => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
