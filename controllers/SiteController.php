48a62cb3aafa731c14ca8216bf4c2713a8f58172
dram1008 <dram1008@yandex.ru>
2015-07-05 16:10:15 +0300

    почта котактов

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -55,7 +55,7 @@ class SiteController extends BaseController
     public function actionContact()
     {
         $model = new ContactForm();
-        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
+        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['mailList']['contact'])) {
             Yii::$app->session->setFlash('contactFormSubmitted');
 
             return $this->refresh();
