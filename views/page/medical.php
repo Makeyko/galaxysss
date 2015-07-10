f8f01ae02b09b842ed2654209310bad7d334f117
Dmitriy Mukhortov <dram1008@yandex.ru>
2015-07-10 12:35:08 +0300

    Верстку поправил

--- views/page/medical.php
+++ views/page/medical.php
@@ -9,29 +9,23 @@ $this->title = 'Союз самоисцеления';
         height: 70px;
     }
 </style>
-    <div class="page-header">
-        <h1>Союз самоисцеления</h1>
+    <div class="col-lg-12">
+        <h1 class="page-header"><?= \yii\helpers\Html::encode($this->title) ?></h1>
+        <p class="lead">Союз занимается восстанавлением
+            здоровья, настраивает тело на резонанс с высшими контурами сознания, регулирует течение внутренних и внешних
+            энергий человека.</p>
+        <p><img src="/images/page/medical/parablev121.jpg" width="100%" class="thumbnail"></p>
     </div>
-    <p class="lead">Союз занимается восстанавлением
-        здоровья, настраивает тело на резонанс с высшими контурами сознания, регулирует течение внутренних и внешних
-        энергий человека.</p>
-
-    <p><img src="/images/page/medical/parablev121.jpg" width="100%" class="thumbnail"></p>
 
 
-    <div class="row">
-        <?= \app\services\GsssHtml::unionCategoryItems(5) ?>
-    </div>
+    <?= \app\services\GsssHtml::unionCategoryItems(5) ?>
+
     <?php if (count($articleList) > 0) { ?>
-        <div class="row">
-            <div class="page-header">
-                <h2>Статьи</h2>
-            </div>
-            <div class="row">
-                <?php foreach ($articleList as $item) {
-                    echo \app\services\GsssHtml::articleItem($item, 'medical');
-                } ?>
-            </div>
+        <div class="col-lg-12">
+            <h2 class="page-header">Статьи</h2>
         </div>
+        <?php foreach ($articleList as $item) {
+            echo \app\services\GsssHtml::articleItem($item, 'medical');
+        } ?>
     <?php } ?>
 </div>
