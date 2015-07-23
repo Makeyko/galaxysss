aa57cab1a6b3dbc5f80d005afb5b8f4bb0fba3bd
Dmitriy Mukhortov <dram1008@yandex.ru>
2015-07-23 12:23:51 +0300

    Добавить рассылку новостей #48

--- commands/SubscribeController.php
+++ commands/SubscribeController.php
@@ -18,38 +18,37 @@ class SubscribeController extends Controller
      */
     public function actionSend()
     {
-        $key = 'app\\commands\\SubscribeController::actionSend';
-        $isCache = \Yii::$app->cache->get($key);
-        if ($key !== false) {
-            \Yii::$app->cache->set($key, true, 60*60);
-            $time = microtime(true);
-            $list = SubscribeMailItem::query()
-                ->limit(10)
-                ->orderBy(['date_insert' => SORT_DESC])
-                ->all();
-            if (count($list) > 0) {
-                \Yii::info('Всего писем для рассылки: ' . count($list), 'gs\\app\\commands\\SubscribeController::actionSend');
-                \Yii::info('Список писем: ' . VarDumper::dumpAsString(ArrayHelper::getColumn($list, 'mail')), 'gs\\app\\commands\\SubscribeController::actionSend');
+        $time = microtime(true);
+        $list = SubscribeMailItem::query()
+            ->limit(10)
+            ->orderBy(['date_insert' => SORT_DESC])
+            ->all();
+        if (count($list) > 0) {
+            \Yii::info('Всего писем для рассылки: ' . count($list), 'gs\\app\\commands\\SubscribeController::actionSend');
+            \Yii::info('Список писем: ' . VarDumper::dumpAsString(ArrayHelper::getColumn($list, 'mail')), 'gs\\app\\commands\\SubscribeController::actionSend');
 
-                foreach($list as $mailItem) {
-                    \Yii::$app->mailer
-                        ->compose()
-                        ->setFrom(\Yii::$app->params['mailer']['from'])
-                        ->setTo($mailItem['mail'])
-                        ->setSubject($mailItem['subject'])
-                        ->setTextBody($mailItem['text'])
-                        ->setHtmlBody($mailItem['html'])
-                        ->send();
+            foreach($list as $mailItem) {
+                $mail = \Yii::$app->mailer
+                    ->compose()
+                    ->setFrom(\Yii::$app->params['mailer']['from'])
+                    ->setTo($mailItem['mail'])
+                    ->setSubject($mailItem['subject'])
+                    ->setHtmlBody($mailItem['html']);
+                if (isset($mailItem['text'])) {
+                    if ($mailItem['text'] != '') {
+                        $mail->setTextBody($mailItem['text']);
+                    }
                 }
-
-                \Yii::info('Список писем для удаления: ' . VarDumper::dumpAsString(ArrayHelper::getColumn($list, 'id')), 'gs\\app\\commands\\SubscribeController::actionSend');
-                SubscribeMailItem::deleteByCondition([
-                    'in', 'id', ArrayHelper::getColumn($list, 'id')
-                ]);
-                \Yii::info('Осталось после рассылки: ' . SubscribeMailItem::query()->count(), 'gs\\app\\commands\\SubscribeController::actionSend');
-                $time = microtime(true) - $time;
-                \Yii::info('Затраченное время на расылку: ' . $time, 'gs\\app\\commands\\SubscribeController::actionSend');
+                $mail->send();
             }
+
+            \Yii::info('Список писем для удаления: ' . VarDumper::dumpAsString(ArrayHelper::getColumn($list, 'id')), 'gs\\app\\commands\\SubscribeController::actionSend');
+            SubscribeMailItem::deleteByCondition([
+                'in', 'id', ArrayHelper::getColumn($list, 'id')
+            ]);
+            \Yii::info('Осталось после рассылки: ' . SubscribeMailItem::query()->count(), 'gs\\app\\commands\\SubscribeController::actionSend');
+            $time = microtime(true) - $time;
+            \Yii::info('Затраченное время на расылку: ' . $time, 'gs\\app\\commands\\SubscribeController::actionSend');
         }
 
         \Yii::$app->end();
