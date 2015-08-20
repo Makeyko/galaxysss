d48b4739bcff744ac7f5ed7b3186ae6f05a2a888
dram1008 <dram1008@yandex.ru>
2015-08-20 21:43:42 +0300

    Привести к стандарту Html Content #303

--- config/urlRules.php
+++ config/urlRules.php
@@ -28,7 +28,7 @@ return [
     'log'                                                                                  => 'site/log',
     'logDb'                                                                                => 'site/log_db',
     'siteUpdate'                                                                           => 'site/site_update',
-    'test'                                                                           => 'site/test',
+    'test'                                                                                 => 'site/test',
 
     'subscribe/unsubscribe'                                                                => 'subscribe/unsubscribe',
     'subscribe/mail'                                                                       => 'subscribe/mail',
