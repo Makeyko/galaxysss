9a1ec71b19146c9046c9c755ca342851a5bed1fa
dram1008 <dram1008@yandex.ru>
2015-08-22 15:03:27 +0300

    Setting unknown property: app\models\Form\Profile::last_action #329

--- models/Form/ProfileSubscribe.php
+++ models/Form/ProfileSubscribe.php
@@ -38,6 +38,7 @@ class ProfileSubscribe extends BaseForm
     public $fb_id;
     public $fb_link;
     public $birth_date;
+    public $last_action;
 
     public $subscribe_is_news;
     public $subscribe_is_site_update;
