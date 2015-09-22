784e095a856af17d32b9a3895ebc68822bd6e7c6
dram1008 <dram1008@yandex.ru>
2015-09-22 13:59:16 +0300

    edit

--- services/investigator/Base.php
+++ services/investigator/Base.php
@@ -61,11 +61,27 @@ class Base
      * @param string $url
      *
      * @return bool|\simple_html_dom
+     * @throws \cs\web\Exception
      */
     public function getDocument($url)
     {
         require_once(\Yii::getAlias('@csRoot/services/simplehtmldom_1_5/simple_html_dom.php'));
 
-        return file_get_html($url);
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36');
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        $body = curl_exec($curl);
+
+        $result = new \StdClass();
+        $result->status_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        $result->body = $body;
+        curl_close($curl);
+
+        if ($result->status_code != 200) {
+            throw new \cs\web\Exception('Не удалось прочитать файл');
+        }
+
+        return str_get_html($body);
     }
 } 
