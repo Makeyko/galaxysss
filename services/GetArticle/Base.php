a098a3256b1f2ef28c46dd89db3660085e4f15d8
dram1008 <dram1008@yandex.ru>
2015-10-05 05:03:42 +0300

    Поправить Следователь #420

--- services/GetArticle/Base.php
+++ services/GetArticle/Base.php
@@ -44,29 +44,30 @@ class Base
     {
         require_once(\Yii::getAlias('@csRoot/services/simplehtmldom_1_5/simple_html_dom.php'));
 
-//        $url = new Url($this->url);
-//        if (strtolower($url->scheme) == 'https') {
-//            $url->scheme = 'http';
-//        }
-//        $url = $url->__toString();
-        $curl = curl_init($this->url);
-        curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36');
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($curl, CURLOPT_HEADER, 0);
-        $body = curl_exec($curl);
-
-        $result = new \StdClass();
-        $result->status_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-        $result->body = $body;
-        curl_close($curl);
-        $d = new \cs\services\SitePath('/upload');
-        $d->addAndCreate('test2');
-        $d->add(time().'_'.Security::generateRandomString().'.txt')->write($body);
-        \Yii::info($body, 'gs\\inv');
-
-        if ($result->status_code != 200) {
-            throw new \cs\web\Exception('Не удалось прочитать файл');
+        $url = new Url($this->url);
+        if (strtolower($url->scheme) == 'https') {
+            $url->scheme = 'http';
         }
+        $url = $url->__toString();
+        $body = file_get_contents($url);
+//        $curl = curl_init($this->url);
+//        curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36');
+//        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+//        curl_setopt($curl, CURLOPT_HEADER, 0);
+//        $body = curl_exec($curl);
+//
+//        $result = new \StdClass();
+//        $result->status_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+//        $result->body = $body;
+//        curl_close($curl);
+//        $d = new \cs\services\SitePath('/upload');
+//        $d->addAndCreate('test2');
+//        $d->add(time().'_'.Security::generateRandomString().'.txt')->write($body);
+//        \Yii::info($body, 'gs\\inv');
+
+//        if ($result->status_code != 200) {
+//            throw new \cs\web\Exception('Не удалось прочитать файл');
+//        }
 
         return str_get_html($body);
     }
