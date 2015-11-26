cc61da306d9ef7c2f4968be34efb7b1516be472f
dram1008 <dram1008@yandex.ru>
2015-11-26 22:59:25 +0300

    hd

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -121,11 +121,7 @@ class SiteController extends BaseController
 
     public function actionTest()
     {
-        $d = "<p><img alt=\"Разумное канализирование\" class=\"thumbnail\" src=\"/upload/FileUpload2/gs_cheneling_list/00000378/original/img.jpg\" style=\"width:100%;\" /></p>";
-        require_once(Yii::getAlias('@csRoot/services/simplehtmldom_1_5/simple_html_dom.php'));
-        $content = str_get_html($d,true,true,'WINDOWS-1251');
-
-        VarDumper::dump($content);
+        VarDumper::dump($_SERVER['SERVER_NAME']);
     }
 
     public function actionStatistic()
